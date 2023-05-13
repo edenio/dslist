0ce7683eecbe29112d13cb25f73803dbe99ee601
Edenio Alves De Oliveira <edenio.oliveira@casaevideo.com.br>
2023-05-13 19:36:18 -0300

    Homologação ok

--- src/main/java/com/devsuperior/dslist/DTO/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/DTO/GameMinDTO.java
@@ -1,66 +1,49 @@
 package com.devsuperior.dslist.DTO;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
-		
+
 	private Long id;
 	private String title;
 	private Integer year;
 	private String imgUrl;
 	private String shortDescription;
 	
-    public GameMinDTO() {
-    }
-    
 	public GameMinDTO(Game entity) {
 		id = entity.getId();
 		title = entity.getTitle();
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
-		shortDescription = entity.getShortDescription();	
+		shortDescription = entity.getShortDescription();
+	}
+
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getGameYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public String getTitle() {
 		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
 	}
 
 	public Integer getYear() {
 		return year;
 	}
 
-	public void setYear(Integer year) {
-		this.year = year;
-	}
-
 	public String getImgUrl() {
 		return imgUrl;
-	}
-
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
 	}
 
 	public String getShortDescription() {
 		return shortDescription;
 	}
-
-	public void setShortDescription(String shortDescription) {
-		this.shortDescription = shortDescription;
-	}
-
-
-
 }
